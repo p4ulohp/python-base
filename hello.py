410e803c643658ede038062dee223381121c9d73
p4ulohp <paulohpirangi@hotmail.com>
2024-02-14 15:00:14 -0300

    created scripts using dict

--- hello.py
+++ hello.py
@@ -15,7 +15,7 @@ Execucao:
             ou  
             ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__  = "Paulo Henrique"
 __license__ = "Unlicense"
 
@@ -23,15 +23,21 @@ import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-msg = "Hello, World!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Ola, Mundo!",
+    "it_IT": "Ciao, Mundo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
+#Ordem de complexidade O(n)
+# if current_language == "pt_BR":
+#     msg = "Ola, Mundo!"
+# elif current_language == "it_IT":
+#     msg = "Ciao, Mondo!"
+# elif current_language == "es_SP":
+#     msg = "Hola, Mundo!"
+# elif current_language == "fr_FR":
+#     msg = "Bonjour Monde"
 
-if current_language == "pt_BR":
-    msg = "Ola, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde"
-
-print(msg)
+print(msg[current_language])
