7ff7b4d8e45995c1fac7c73362ac8eb8aee81a2a
p4ulohp <paulohpirangi@hotmail.com>
2024-02-16 06:15:04 -0300

    created infixcalc v0.1.0

---
+++ infixcalc.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""Calculadora infix.]
+
+Funcionamento:
+
+[operacao] [n1] [n2]
+
+Operacaoes:
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Uso:
+$ infixcalc.py sum 5 2
+7
+
+$ infixcalc.py mul 10 5
+50
+
+$ infixcalc.py
+opracao: sum 
+n1: 5
+n2: 4
+9
+"""
+
+__version__ = "0.1.0"
+
+import sys   #pegar os argumentos
+
+arguments = sys.argv[1:]
+
+if not arguments:
+    operation = input("Operacao:")
+    n1 = input("n1:")
+    n2 = input("n2:")
+    arguments = [operation, n1, n2]
+
+elif len(arguments) != 3:
+    print("Numero de argumentos invalidos")
+    print("ex: 'sum 5 5'")
+    sys.exit(1)
+
+operation, *nums = arguments
+
+valid_operations = ("sum", "sub", "mul", "div")
+if operation not in valid_operations:
+    print("Operaco invalida")
+    print(valid_operations)
+    sys.exit(1)
+
+validated_nums = []
+for num in nums:
+    if not num.replace(".", "").isdigit():
+        print(f"Numero invalido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+    validated_nums.append(num)
+
+n1, n2 = validated_nums
+
+if operation == "sum":
+    result = n1 + n2
+elif operation == "sum":
+    result = n1 - n2
+elif operation == "mul":
+    result = n1 * n2
+elif operation == "div":
+    result = n1 / n2 
+
+print(f"O resultado é {result}") 
