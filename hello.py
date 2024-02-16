42fdffa74d6663d004133ec64f068080c25bb490
p4ulohp <paulohpirangi@hotmail.com>
2024-02-16 05:24:00 -0300

    created hello-world with disct

--- hello.py
+++ hello.py
@@ -10,18 +10,51 @@ Tenha a variavel LANG devidamente configurada, ex:
 
             export LANG=pt_BR
 
+Ou informe atraves do CLI argument '--lagn'
+
+Ou o usuario tera que digitar.
+
 Execucao:
             python3 hello.py
             ou  
             ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__  = "Paulo Henrique"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+
+for arg in sys.argv[1:]:
+    #TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option '{key}'")
+        sys.exti()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    #TODO: Usar repeticao
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    
+    else:
+        current_language = input(
+            "Choose a language:"
+        )
+
+
+current_language = current_language[:5]
 
 msg = {
     "en_US": "Hello, World!",
@@ -40,4 +73,4 @@ msg = {
 # elif current_language == "fr_FR":
 #     msg = "Bonjour Monde"
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
